cb3d7e7fe568b9835dc4053f03ffc39859156052
FurVie <154724311+FurVie@users.noreply.github.com>
2025-12-08 21:28:49 +0700

    Fix some stuff

--- Midterm/http_proxy_mpi.c
+++ Midterm/http_proxy_mpi.c
@@ -6,10 +6,29 @@
 
 #define BUFFER_SIZE 4096
 
+// --- HÀM MỚI: Callback để nhận dữ liệu từ Web Server an toàn ---
+size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
+    size_t realsize = size * nmemb;
+    char *response_buffer = (char *)userp;
+
+    // Kiểm tra để tránh tràn bộ đệm
+    if (realsize >= BUFFER_SIZE) {
+        printf("Warning: Response too large, truncating...\n");
+        realsize = BUFFER_SIZE - 1;
+    }
+
+    // Copy dữ liệu nhận được vào biến response
+    memcpy(response_buffer, contents, realsize);
+    
+    // Thêm ký tự kết thúc chuỗi
+    response_buffer[realsize] = 0;
+
+    return realsize;
+}
+
 void send_http_request(const char* request, char* response) {
     CURL* curl;
     CURLcode res;
-    struct curl_slist* headers = NULL;
 
     curl = curl_easy_init();
     if (!curl) {
@@ -19,18 +38,21 @@ void send_http_request(const char* request, char* response) {
     }
 
     // Configure the HTTP request
-    curl_easy_setopt(curl, CURLOPT_URL, "https://webhook.site/e5022f2e-5e9a-4290-9f06-cb1c906a5d78");
+    // LƯU Ý: URL này là ví dụ, bạn nên lấy URL mới từ https://webhook.site để test chính xác nhất
+    curl_easy_setopt(curl, CURLOPT_URL, "https://webhook.site/d5ea60ce-050e-47c7-b77c-bd16907f0665");
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request);
 
-    // Set up a response handler
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, memcpy);
+    // --- PHẦN SỬA LỖI ---
+    // Thay vì dùng memcpy trực tiếp, ta dùng hàm write_callback
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
+    // --------------------
 
     // Perform the HTTP request
     res = curl_easy_perform(curl);
     if (res != CURLE_OK) {
         fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        strcpy(response, "HTTP request failed");
+        sprintf(response, "HTTP request failed: %s", curl_easy_strerror(res));
     }
 
     curl_easy_cleanup(curl);
@@ -61,7 +83,7 @@ int main(int argc, char** argv) {
     }
 
     if (world_rank == 0) {
-        // Client process: Decide whether to send a custom message or a file
+        // --- CLIENT PROCESS ---
         char http_request[BUFFER_SIZE] = {0};
         
         if (argc == 2) {
@@ -70,7 +92,9 @@ int main(int argc, char** argv) {
             printf("Client is sending file content: %s\n", http_request);
         } else {
             // Default message input
-            printf("Enter a message to send: ");
+            printf("Enter a message to send: \n");
+            fflush(stdout); // QUAN TRỌNG: Đẩy dòng chữ ra màn hình ngay lập tức
+            
             fgets(http_request, BUFFER_SIZE, stdin);
             http_request[strcspn(http_request, "\n")] = 0;  // Remove trailing newline
         }
@@ -81,8 +105,9 @@ int main(int argc, char** argv) {
         char http_response[BUFFER_SIZE];
         MPI_Recv(http_response, BUFFER_SIZE, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Client received response: %s\n", http_response);
+
     } else if (world_rank == 1) {
-        // Proxy process: Receive HTTP request from client
+        // --- PROXY PROCESS ---
         char http_request[BUFFER_SIZE];
         MPI_Recv(http_request, BUFFER_SIZE, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
